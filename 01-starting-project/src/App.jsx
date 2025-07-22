c58f06c9370020661c73f78e829f15804b9dd5f8
LINKTECH\fernando.gallegos <fernando.gallegos@linktech.com.mx>
2025-07-22 09:53:16 -0600

    commit inicial

---
+++ 01-starting-project/src/App.jsx
@@ -0,0 +1,53 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  let ret = Math.floor(Math.random() * (max + 1));
+  console.log(ret);
+  return ret;
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)]
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept(){
+  return (
+    <li>
+      <img src="..." alt="..." />
+      <h3>TITLE</h3>
+      <p>DESCRIPTION</p>
+    </li> 
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts"></section>
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept title="Components" 
+          description="The core UI building block." 
+          image={componentsImg}
+          />
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
