2c90b985c8dd3a2070551137b66fa7c5ed609d4b
Juan Gallegos <juanf.2890@gmail.com>
2025-08-19 11:46:16 -0600

    - inicialize tic tac toe proyect

--- 01-starting-project/src/components/Tabs.jsx
+++ 01-starting-project/src/components/Tabs.jsx
@@ -1,10 +1,10 @@
-export default function Tabs({children, buttons, ButtonsContainer = 'menu'}){
+export default function Tabs({children, buttons, buttonsContainer}){
   
   //We want to use the value thats received on the buttonsContainer prop
   //as the identifier of the component.
 
   //We create a variable which can be used as a custom component
-  //const ButtonsContainer = buttonsContainer;
+  const ButtonsContainer = buttonsContainer;
   
   return(
     <>
