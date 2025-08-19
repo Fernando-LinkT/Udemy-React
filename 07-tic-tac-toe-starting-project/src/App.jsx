b5abb002662498306c0d8cc187f5b5eb24f54f87
Juan Gallegos <juanf.2890@gmail.com>
2025-08-19 13:10:29 -0600

    - two-way-binding for the inputs of the players

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,19 +1,16 @@
-import Player from "./components/Player"
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
     <main>
       <div id="game-container">
-        <ol id="players" >
-          <Player name="Player 1" symbol="X"/>
-          <Player name="Player 2" symbol="O"/>
-          
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,49 @@
-export default function Player({name, symbol}){
-  return(
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    /*
+    if (isEditing) {
+      setIsEditing(false);
+    } else {
+      setIsEditing(true);
+    } Es mejor escribir esto ↓ */
+    /*setIsEditing(!isEditing);*/
+
+    //aun mejor es escribir esto
+    setIsEditing((editing) => !editing);
+  }
+
+  let playerNameHTMLCode = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    playerNameHTMLCode = (
+      //THIS IS CALLED TO-WAY-BINDING because we are getting a value out
+      //and we are feeding a value into the input
+      <input
+        type="text"
+        required
+        onChange={handleChange}
+        value={playerName}
+      ></input>
+    );
+  }
+
+  function handleChange(event) {
+    //aqui se obtiene el acceso a lo que el usuario escribio
+    setPlayerName(event.target.value);
+  }
+
+  return (
     <li>
       <span className="player">
-        <span className="player-name" >{name}</span>
+        {playerNameHTMLCode}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
-  )
+  );
 }
